5d29608f6bd2757a4d449065dde57a89d7d8a3b6
dsplayerX <dumindu0043@gmail.com>
2023-02-11 12:47:19 +0530

    removed 2 flask app instances

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
-app = Flask(__name__)
-  
+
 # Route for seeing a data
 @app.route('/data')
 def get_time():
-  
     # Returning an api for showing in  reactjs
     return {
         'Name':"geek", 
