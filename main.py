f90a81f6c0c269fdf3d24700a7261add6b25ba05
dsplayerX <dumindu0043@gmail.com>
2023-02-12 16:19:02 +0530

    Squashed commit of the following:
    commit b4ce2caf3d33f0ae13ac4ff159d61d04f042075f
    Author: TharU0418 <101709936+TharU0418@users.noreply.github.com>
    Date:   Sun Feb 12 10:37:40 2023 +0530
    
        added pandas to requirements.txt
    
    commit b767f9c097b9ca69bc41ab009e94c2d6d6cded55
    Author: TharU0418 <101709936+TharU0418@users.noreply.github.com>
    Date:   Sun Feb 12 10:26:41 2023 +0530
    
        integrated youtube api to fetch comments
    
        error - only returns 100 comments even if maxResults was higher
    
    commit a9791af7c8b0e8e42d9879a8ba4d3bf351d8ae72
    Author: dsplayerX <dumindu0043@gmail.com>
    Date:   Sun Feb 12 09:58:02 2023 +0530
    
        created two routes in api

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
 from flask import Flask, jsonify
 import os
+from googleapiclient.discovery import build
+import pandas as pd
+
+api_service_name = "youtube"
+api_version = "v3"
+api_key = ""
+
+video_id = ""
+
+resourse = build('youtube','v3', developerKey = api_key)
+
+
+request = resourse.commentThreads().list(
+                                part="snippet",
+                                videoId=video_id,
+                                maxResults=100,  # only returns 100 comments (even if this value was higher)
+                                order="relevance")
+
+response = request.execute()
+
+items = response["items"]
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
     
-# Route for seeing a data
-@app.route('/data')
-def testdata():
-    # Returning an api for showing in json
-    return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":"x", 
-        "programming":"python"
-        }
+@app.route('/analyse')
+def index():
+
+    data = []
+    for item in items:
+        item_info = item["snippet"]
+        topLevelComment = item_info["topLevelComment"]
+        comment_info = topLevelComment["snippet"]
+    
+        data.append({
+            "Comment By": comment_info["authorDisplayName"],
+            "Comment Text": comment_info["textDisplay"],
+            "Likes on Comment": comment_info["likeCount"],
+            "Comment Date": comment_info["publishedAt"]
+        })
+
+    df = pd.DataFrame(data)
+    df.to_csv("/temp/temp_comments.csv", encoding="utf-8")
+
+    return df.to_json
+
+@app.route('/results')
+def index():
+    return jsonify({"Results Route"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
